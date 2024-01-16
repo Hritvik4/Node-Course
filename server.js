52f40d6796e1fa31efb403956f7454858024159a
Hritvik4 <hritvik0408@gmail.com>
2024-01-16 12:23:46 +0530

    Routing using express

--- server.js
+++ server.js
@@ -1,54 +1,61 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
+// const _ = require('lodash');
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method);
+// const server = http.createServer((req,res) => {
 
-    let path = './views/';
-    switch(req.url)
-    {
-        case '/':
-            path+= 'index.html'
-            res.statusCode = 200;
-            break;
-        case '/about':
-            path+= 'about.html'
-            res.statusCode = 200;
-            break;
-        case '/about-me':
-            res.statusCode = 301;
-            res.setHeader('Location', '/about');
-            res.end();
-            break;
-        default:
-            path +='404.html'
-            res.statusCode = 404;
-            break;
-    }
+
+//     // console.log(req.url, req.method); 
+
+//     //lodash
+//     const num = _.random(0,20);  
+//     console.log(num);
+
+//     let path = './views/';
+//     switch(req.url)
+//     {
+//         case '/':
+//             path+= 'index.html'
+//             res.statusCode = 200;
+//             break;
+//         case '/about':
+//             path+= 'about.html'
+//             res.statusCode = 200;
+//             break;
+//         case '/about-me':
+//             res.statusCode = 301;
+//             res.setHeader('Location', '/about');
+//             res.end();
+//             break;
+//         default:
+//             path +='404.html'
+//             res.statusCode = 404;
+//             break;
+//     }
     
-    //set header content type
-    res.setHeader('Content-Type', 'text/html');
+//     //set header content type
+//     res.setHeader('Content-Type', 'text/html');
 
-    //send an HTML file
-    fs.readFile(path, (err, data) => {
-        if(err)
-        {
-            console.log(err);
-            res.end();
-        }
-        else
-        {
-            // if using write only once, then directly pass in end()
-            res.end(data);
+//     //send an HTML file
+//     fs.readFile(path, (err, data) => {
+//         if(err)
+//         {
+//             console.log(err);
+//             res.end();
+//         }
+//         else
+//         {
+//             // if using write only once, then directly pass in end()
+//             res.end(data);
             
-            // res.write(data);
-            // res.end();
-        }
-    });
+//             // res.write(data);
+//             // res.end();
+//         }
+//     });
 
     
-});
+// });
 
-server.listen(3000, 'localhost', ()=>{
-    console.log('Listening for requests on port 3000');
-});
+// server.listen(3000, 'localhost', ()=>{
+//     console.log('Listening for requests on port 3000');
+// });
